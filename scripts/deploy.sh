ca198be13519637bc8c68120c4607185eb59135e
Jerome Gravel-Niquet <jeromegn@gmail.com>
2017-12-29 13:53:18 -0500

    fail curl if response is not 2xx

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 flybaseurl=${FLY_BASE_URL:-"https://fly.io"}
 
-curl -i -X PUT -F 'script=@dist/documentup.js' \
+curl --fail -i -X PUT -F 'script=@dist/documentup.js' \
   -H "Authorization: Bearer $FLY_ACCESS_TOKEN" \
   $flybaseurl/api/v1/sites/js-documentup-com/script
