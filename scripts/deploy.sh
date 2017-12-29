6620c2e457d9104e1a473cb76c402b354f8ae892
Jerome Gravel-Niquet <jeromegn@gmail.com>
2017-12-29 13:36:12 -0500

    proper site slug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ flybaseurl=${FLY_BASE_URL:-"https://fly.io"}
 
 curl -i -X PUT -F 'script=@dist/documentup.js' \
   -H "Authorization: Bearer $FLY_ACCESS_TOKEN" \
-  $flybaseurl/api/v1/sites/documentup-test/script
+  $flybaseurl/api/v1/sites/js-documentup-com/script
