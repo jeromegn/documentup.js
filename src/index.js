645aac2be6344855fad119e4ea3e4e1fe8fd108a
Jerome Gravel-Niquet <jeromegn@gmail.com>
2018-01-27 14:09:06 -0500

    PoC code docs

--- src/index.js
+++ src/index.js
@@ -4,24 +4,29 @@ const Renderer = require('./renderer')
 const Repository = require('./repository')
 
 const pageTpl = require('./views/page.pug')
+const codeTpl = require('./views/code.pug')
 
 var router = Router()
 
-router.addRoute("/screen.css", function (event, params) {
+router.addRoute("/screen.css", function (event, match) {
   return new Response(css, { headers: { 'content-type': 'text/css' } })
 })
 
-router.addRoute("/", async function (event, params) {
+router.addRoute("/", async function (event, match) {
   return await renderRepo("superfly", "documentup.js")
 })
 
-router.addRoute("/:login/:repo", async function (event, params) {
-  return await renderRepo(params.login, params.repo)
+router.addRoute("/:login/:repo", async function (event, match) {
+  return await renderRepo(match.params.login, match.params.repo)
+})
+
+router.addRoute("/:login/:repo/*.*", async function (event, match) {
+  return await renderCode(match.params.login, match.params.repo, match.splats.join("."))
 })
 
 async function renderRepo(login, repoName) {
   const response = await fetch(`https://raw.githubusercontent.com/${login}/${repoName}/master/README.md`)
-  const renderFn = async function(){
+  const renderFn = async function () {
     const repo = new Repository(login, repoName)
     const renderer = new Renderer(login, repoName)
     let result = renderer.render(await response.text())
@@ -29,17 +34,17 @@ async function renderRepo(login, repoName) {
   }
   let cacheStatus = "MISS"
 
-  if(response.status != 200){
-    return new Response("four-oh-four", {status: 404})
+  if (response.status != 200) {
+    return new Response("four-oh-four", { status: 404 })
   }
   const key = login + "/" + repoName + ":" + response.headers.get("etag")
   let body = await fly.cache.getString(key)
 
-  if(!body){
+  if (!body) {
     console.log("cache miss:", key)
     body = await renderFn()
     await fly.cache.set(key, body, 3600)
-  }else{
+  } else {
     console.log("cache hit:", key)
     cacheStatus = "HIT"
   }
@@ -47,17 +52,28 @@ async function renderRepo(login, repoName) {
   return new Response(body, { headers: { 'content-type': 'text/html', 'x-cache': cacheStatus } })
 }
 
-console.log("before fetch")
+const commentExtractor = require('multilang-extract-comments')
+const splitLines = require('split-lines')
+const arrayToLinkedlist = require('array-to-linkedlist')
+
+async function renderCode(login, repoName, filePath) {
+  const response = await fetch(`https://raw.githubusercontent.com/${login}/${repoName}/master/${filePath}`)
+  const source = await response.text()
+  const comments = commentExtractor(source)
+
+  const body = codeTpl({ comments: arrayToLinkedlist(Object.values(comments)), source: splitLines(source) })
+
+  return new Response(body, { headers: { 'content-type': 'text/html' } })
+}
 
 addEventListener("fetch", function (event) {
   const { request, respondWith } = event
   const path = new URL(request.url).pathname
   let match = router.match(path)
 
-  if (!match){
+  if (!match) {
     event.respondWith(new Response("docup not found", { status: 404 }))
-  }else{
-    event.respondWith(match.fn(event, match.params))
+  } else {
+    event.respondWith(match.fn(event, match))
   }
 })
-console.log("after fetch")
